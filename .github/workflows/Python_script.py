738b315be5e5c7a6299176983e16105acf8dd4b6
Adhavan <104341988+AdhavanHero@users.noreply.github.com>
2023-06-06 11:05:46 +0530

    Update Python_script.py

--- .github/workflows/Python_script.py
+++ .github/workflows/Python_script.py
@@ -3,8 +3,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
 
-email = os.environ.get('EMAIL')
-api_key = os.environ.get('API_KEY')
+email = os.env.get('EMAIL')
+api_key = os.env.get('API_KEY')
 print(email)
 print(api_key)
 
