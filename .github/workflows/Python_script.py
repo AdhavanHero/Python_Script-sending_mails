705cf239033fa4d852e460c27b94cdc34eb20da0
Adhavan <104341988+AdhavanHero@users.noreply.github.com>
2023-06-06 11:22:20 +0530

    Update Python_script.py

--- .github/workflows/Python_script.py
+++ .github/workflows/Python_script.py
@@ -10,8 +10,8 @@ api_key = os.env.get('API_KEY')
 # Set up the SMTP server
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
-smtp_username = email
-smtp_password = api_key
+smtp_username = 'superhero8871@gmail.com'
+smtp_password = 'nythowumurzjlvoq'
 # Create a message object and set the message content
 
 my_array = [
