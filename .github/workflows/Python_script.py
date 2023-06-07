d6da214d60aaf19547ef45134bfd3f2b6f72885a
Adhavan <104341988+AdhavanHero@users.noreply.github.com>
2023-06-07 15:42:31 +0530

    Update Python_script.py

--- .github/workflows/Python_script.py
+++ .github/workflows/Python_script.py
@@ -9,4 +9,38 @@ email = os.environ.get('Email')
 print(api_key)
 Print(email)
 
+# Set up the SMTP server
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = email
+smtp_password = api_key
+
+def message(Var1, Var2, Var3):
+    message = MIMEMultipart()
+    message["From"] = email
+    message["To"] = Var2
+    message["Subject"] = Var1
+    message_text = Var3
+    message.attach(MIMEText(message_text))
+
+    smtp_client = smtplib.SMTP(smtp_server, smtp_port)
+    smtp_client.ehlo()
+    smtp_client.starttls()
+    smtp_client.login(smtp_username,smtp_password)
+    smtp_client.sendmail(message["From"], message["To"], message.as_string())
+    smtp_client.quit() 
+
+my_array = [
+    {"email": "ap4992@srmist.edu.in", "Cont": "Hi dude", "mission": "save the world"},
+    {"email": "adhavanponram@gmail.com", "Cont": "Hi Adhavan", "mission": "Never give up"},
+    {"email": "Superhero8871@gmail.com", "Cont": "Hi Hero", "mission": "win the war at any cost"}
+]
+
+for item in my_array:
+    message(item["mission"], item["email"], item["Cont"])
+    
+print('Array printed!')    
+
+
+
 
